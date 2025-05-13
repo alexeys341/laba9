5126e1d171e8e14013d4f38658afdb511f7d6aee
alexeys341 <154486139+alexeys341@users.noreply.github.com>
2025-05-13 21:31:29 +0500

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,174 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab9;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// Данный класс содержит методы, которые отвечают за нажатия кнопок в интерфейсе
+/// Метод CheckCorrection1 проверяет корректность первого треугольника
+/// Метод CheckCorrection2 проверяет корректность второго треугольника
+/// Метод CalculatePerimetr1 считает периметр первого треугольника
+/// Метод CalculatePerimetr2 считает периметр второго треугольника
+/// Метод CalculateSquare1 считает площадь первого треугольника
+/// Метод CalculateSquare2 считает площадь второго треугольника
+/// Метод CompareSquares сравнивает площади треугольников
+/// </summary>
+public partial class MainWindow : Window
+{
+    private void CheckCorrection1(object sender, RoutedEventArgs e)
+    {
+        string sideAOfFirstTriangle = SideA1TextBox.Text;
+        string sideBOfFirstTriangle = SideB1TextBox.Text;
+        string sideCOfFirstTriangle = SideC1TextBox.Text;
+        Triangles triangle1 = new Triangles();
+        bool correct = triangle1.Check(sideAOfFirstTriangle, sideBOfFirstTriangle, sideCOfFirstTriangle);
+        if (correct == false)
+        {
+            ResultTextBox.Clear();
+            ResultTextBox.SelectedText = "Incorrect value or triangle doesn't exist";
+        }
+        else
+        {
+            ResultTextBox.Clear();
+            ResultTextBox.SelectedText = "triangle exist";
+        }
+    }
+
+    private void CheckCorrection2(object sender, RoutedEventArgs e)
+    {
+        string sideAOfSecondTriangle = SideA2TextBox.Text;
+        string sideBOfSecondTriangle = SideB2TextBox.Text;
+        string sideCOfSecondTriangle = SideC2TextBox.Text;
+        Triangles triangle2 = new Triangles();
+        bool correct = triangle2.Check(sideAOfSecondTriangle, sideBOfSecondTriangle, sideCOfSecondTriangle);
+        if (correct == false)
+        {
+            ResultTextBox.Clear();
+            ResultTextBox.SelectedText = "Incorrect value or triangle doesn't exist";
+        }
+        else
+        {
+            ResultTextBox.Clear();
+            ResultTextBox.SelectedText  = "triangle exist";
+        }
+    }
+
+    private void CalculatePerimetr1(object sender, RoutedEventArgs e)
+    {
+        string sideAOfFirstTriangle = SideA1TextBox.Text;
+        string sideBOfFirstTriangle = SideB1TextBox.Text;
+        string sideCOfFirstTriangle = SideC1TextBox.Text;
+        Triangles triangle1 = new Triangles();
+        bool correct = triangle1.Check(sideAOfFirstTriangle, sideBOfFirstTriangle, sideCOfFirstTriangle);
+        if (correct == false)
+        {
+            ResultTextBox.Clear();
+            ResultTextBox.SelectedText = "Incorrect value or triangle doesn't exist";
+        }
+        else
+        {
+            ResultTextBox.Clear();
+            ResultTextBox.SelectedText = "Perimetr of first triangle is: " + triangle1.ToString(triangle1);
+        }
+    }
+
+    private void CalculatePerimetr2(object sender, RoutedEventArgs e)
+    {
+        string sideAOfSecondTriangle = SideA2TextBox.Text;
+        string sideBOfSecondTriangle = SideB2TextBox.Text;
+        string sideCOfSecondTriangle = SideC2TextBox.Text;
+        ResultTextBox.Clear();
+        Triangles triangle2 = new Triangles();
+        bool correct = triangle2.Check(sideAOfSecondTriangle, sideBOfSecondTriangle, sideCOfSecondTriangle);
+        if (correct == false)
+        {
+            ResultTextBox.SelectedText = "Incorrect value or triangle doesn't exist";
+        }
+        else
+        {
+            ResultTextBox.SelectedText = "Perimetr of first triangle is: " + triangle2.ToString(triangle2);
+        }
+    }
+
+    private void CalculateSquare1(object sender, RoutedEventArgs e)
+    {
+        string sideAOfFirstTriangle = SideA1TextBox.Text;
+        string sideBOfFirstTriangle = SideB1TextBox.Text;
+        string sideCOfFirstTriangle = SideC1TextBox.Text;
+        ResultTextBox.Clear();
+        Triangles triangle1 = new Triangles();
+        bool correct = triangle1.Check(sideAOfFirstTriangle, sideBOfFirstTriangle, sideCOfFirstTriangle);
+        if (correct == false)
+        {
+            ResultTextBox.SelectedText = "Incorrect value or triangle doesn't exist";
+        }
+        else
+        {
+            ResultTextBox.SelectedText = "Square of first triangle is: " + triangle1.ToString(-triangle1);
+        }
+    }
+
+    private void CalculateSquare2(object sender, RoutedEventArgs e)
+    {
+        string sideAOfSecondTriangle = SideA2TextBox.Text;
+        string sideBOfSecondTriangle = SideB2TextBox.Text;
+        string sideCOfSecondTriangle = SideC2TextBox.Text;
+        ResultTextBox.Clear();
+        Triangles triangle2 = new Triangles();
+        bool correct = triangle2.Check(sideAOfSecondTriangle, sideBOfSecondTriangle, sideCOfSecondTriangle);
+        if (correct == false)
+        {
+            ResultTextBox.SelectedText = "Incorrect value or triangle doesn't exist";
+        }
+        else
+        {
+            ResultTextBox.SelectedText = "Square of second triangle is: " + triangle2.ToString(-triangle2);
+        }
+    }
+
+    private void CompareSquares(object sender, RoutedEventArgs e)
+    {
+        string sideAOfSecondTriangle = SideA2TextBox.Text;
+        string sideBOfSecondTriangle = SideB2TextBox.Text;
+        string sideCOfSecondTriangle = SideC2TextBox.Text;
+        Triangles triangle2 = new Triangles();
+        bool correct1 = triangle2.Check(sideAOfSecondTriangle, sideBOfSecondTriangle, sideCOfSecondTriangle);//правильность первого треугольника
+
+        ResultTextBox.Clear();
+
+        string sideAOfFirstTriangle = SideA1TextBox.Text;
+        string sideBOfFirstTriangle = SideB1TextBox.Text;
+        string sideCOfFirstTriangle = SideC1TextBox.Text;
+        Triangles triangle1 = new Triangles();
+
+        bool correct2 = triangle1.Check(sideAOfFirstTriangle, sideBOfFirstTriangle, sideCOfFirstTriangle);//правильность второго треугольника
+        if ((correct1 == false) || (correct2 == false))
+        {
+            ResultTextBox.SelectedText = "Incorrect value or triangle doesn't exist";
+        }
+        else
+        {
+            if (triangle1 > triangle2)
+            {
+                ResultTextBox.SelectedText = "First triangle have bigger square then second triangle";
+            }
+            else if (triangle1 < triangle2)
+            {
+                ResultTextBox.SelectedText = "Second triangle have bigger square then first triangle";
+            }
+            else
+            {
+                ResultTextBox.SelectedText = "triangles have equal squares";
+            }
+        }
+    }
+}
---
+++ Triangles.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Security.Policy;
+using System.Windows.Controls;
+using System.Xml.Linq;
+
+namespace lab9
+{
+
+    /// <summary>
+    /// Класс называется Triangles потому что так написано в задании, не бейте меня за это
+    /// Класс состоит из 3 полей - сторон треугольника и различных методов и переопределений:
+    /// метод Give - заполняет поля класса
+    /// метод Check - проверка корректности, возвращает true при существовании треугольника, иначе false
+    /// перегрузка оператора "-"  - подсчет площади треугольника - возвращает площадь, если треугольник существует, иначе - 0
+    /// перегрузка bool - возвращает true, если данные стороны образуют треугольник, иначе вернет false
+    /// перегрузка double - возвращает периметр треугольника при его существовании, иначе вернет 0
+    /// перегрузка оператора ">" - возвращает true, если площадь первого треугольника больше, чем площадь второго треугольника, иначе вернет false
+    /// перегрузка оператора "<" - возвращает true, если площадь второго треугольника больше, чем площадь первого треугольника, иначе вернет false
+    /// </summary>
+    class Triangles
+    {
+        private double _a = 0;
+        private double _b = 0;
+        private double _c = 0;
+
+        public static double operator -(Triangles trial)
+        {
+            double a = trial._a;
+            double b = trial._b;
+            double c = trial._c;
+
+            if ((a + b > c) && (b + c > a) && (a + c > b))
+            {
+                double p = (a + b + c) / 2;
+                double Square = p * (p - a) * (p - b) * (p - c);
+                Square = Math.Sqrt(Square);
+                return Square;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public static explicit operator bool(Triangles trial)
+        {
+            double a = trial._a;
+            double b = trial._b;
+            double c = trial._c;
+            if ((a + b > c) && (b + c > a) && (a + c > b))
+            {
+                Console.WriteLine("Такой треугольник существует");
+                return true;
+            }
+            else if ((a + b == c) || (b + c == a) || (a + c == b))
+            {
+                Console.WriteLine("Это не треугольник, а прямая линия");
+                return false;
+            }
+            Console.WriteLine("Это не треугольник, а какие-то три отрезка");
+            return false;
+        }
+
+        public static implicit operator double(Triangles trial)
+        {
+            double a = trial._a;
+            double b = trial._b;
+            double c = trial._c;
+            if ((a + b > c) && (b + c > a) && (a + c > b))
+            {
+                double Perimetr = a + b + c;
+                return Perimetr;
+            }
+            return 0;
+        }
+
+        public static bool operator >(Triangles one, Triangles two) // тут triangles one и two это просто треугольник 1 и треугольник 2, их надо сравнить
+        {
+            double squareFirst;
+            double squareSecond;
+
+            if ((one._a + one._b > one._c) && (one._b + one._c > one._a) && (one._a + one._c > one._b))
+            {
+                double p = (one._a + one._b + one._c) / 2;
+                squareFirst = p * (p - one._a) * (p - one._b) * (p - one._c);
+                squareFirst = Math.Sqrt(squareFirst);
+            }
+            else
+            {
+                squareFirst = 0;
+            }
+
+            if ((two._a + two._b > two._c) && (two._b + two._c > two._a) && (two._a + two._c > two._b))
+            {
+                double p = (two._a + two._b + two._c) / 2;
+                squareSecond = p * (p - two._a) * (p - two._b) * (p - two._c);
+                squareSecond = Math.Sqrt(squareSecond);
+            }
+            else
+            {
+                squareSecond = 0;
+            }
+
+            if (squareFirst > squareSecond)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator <(Triangles one, Triangles two)//тут triangles one и two это просто треугольник 1 и треугольник 2, их надо сравнить
+        {
+            double squareFirst;
+            double squareSecond ;
+
+            if ((one._a + one._b > one._c) && (one._b + one._c > one._a) && (one._a + one._c > one._b))
+            {
+                double p = (one._a + one._b + one._c) / 2;
+                squareFirst = p * (p - one._a) * (p - one._b) * (p - one._c);
+                squareFirst = Math.Sqrt(squareFirst);
+            }
+            else
+            {
+                squareFirst = 0;
+            }
+
+            if ((two._a + two._b > two._c) && (two._b + two._c > two._a) && (two._a + two._c > two._b))
+            {
+                double p = (two._a + two._b + two._c) / 2;
+                squareSecond = p * (p - two._a) * (p - two._b) * (p - two._c);
+                squareSecond = Math.Sqrt(squareSecond);
+            }
+            else
+            {
+                squareSecond = 0;
+            }
+
+            if (squareFirst < squareSecond)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public string ToString(double something)
+        {
+            string line = something + "";
+            return line;
+        }
+
+        private void Give(double a, double b, double c)
+        {
+            _a = a;
+            _b = b;
+            _c = c;
+        }
+
+        public bool Check(string a, string b, string c)
+        {
+            double correctA;
+            double correctB;
+            double correctC;
+            if ((a == "") || (b == "") || (c == ""))
+            {
+                return false;
+            }
+
+            try
+            {
+                correctA = Double.Parse(a);
+                try
+                {
+                    correctB = Int32.Parse(b);
+                    try
+                    {
+                        correctC = Int32.Parse(c);
+                    }
+                    catch (FormatException)
+                    {
+                        return false;
+                    }
+                }
+                catch (FormatException)
+                {
+                    return false;
+                }
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+            if ((correctA + correctB > correctC) && (correctB + correctC > correctA) && (correctA + correctC > correctB))
+            {
+                Give(correctA, correctB, correctC);
+                return true;
+            }
+            return false;
+        }
+
+        /*public void check(Triangles class_name)
+        {
+            a = class_name.a;
+            b = class_name.b;
+            c = class_name.c;
+
+            if ((a + b > c) && (b + c > a) && (a + c > b))
+            {
+                Console.WriteLine("Такой треугольник существует");
+            }
+            else if ((a + b == c) || (b + c == a) || (a + c == b))
+            {
+                Console.WriteLine("Это не треугольник, а прямая линия");
+            }
+            else
+            {
+                Console.WriteLine("Это не треугольник, а какие-то три отрезка");
+            }
+        }*/
+    }
+}
+
+
